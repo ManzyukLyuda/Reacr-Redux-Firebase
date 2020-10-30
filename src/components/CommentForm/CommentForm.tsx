43f17a9af0f8a19c7ab48263c7537386a6bba9f1
Liuda Manziuk <manzzyuk@gmail.com>
2020-10-30 12:12:48 +0200

    Comment thread added

---
+++ src/components/CommentForm/CommentForm.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { useForm } from "react-hook-form";
+import User from '../../models/User';
+import { useDispatch } from 'react-redux';
+import { addComment } from '../../actions';
+
+interface Props{
+    todoId: number
+    user: User,
+}
+
+
+const CommentForm: React.FC<Props>= (props: Props) => {
+    const { register, handleSubmit, formState, reset } = useForm({ mode: 'onChange' });
+    const dispatch  = useDispatch();
+
+    const handlerSubmitComment = (data: any) => {
+        const  { todoId, user } = props
+
+        dispatch(addComment(user.email, data.comment, todoId))
+        reset();
+    }
+
+    return  <form onSubmit={handleSubmit(handlerSubmitComment)} className="comment-form">
+                <textarea name="comment" placeholder="Leave comment" ref={register({ required: true })}></textarea>
+                <button type='submit' disabled={!formState.isValid} className="btn">Comment</button>
+            </form>
+}
+
+export default CommentForm;
